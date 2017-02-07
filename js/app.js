627421cf92df0bb3ae9a2a4fb860acd126cd8ca8
lishican1 <1213782659@qq.com>
2017-02-07 18:11:13 +0800

    KKK

--- js/app.js
+++ js/app.js
@@ -1,21 +1,32 @@
 window.onload = (function(win) {
 
+    var isSwiper = true
+
+    //执行加载
     loading()
 
+    //加载函数
     function loading(){
       $('body').css('overflow','hidden')
-      setTimeout(function(){
-        $('.loading').css({display:'none',background:'rgba(0,0,0,0.7)'})
-        $('body').css('overflow','auto')
-        init()
-      },2000)
+      initPage()
     }
 
-    function init() {
-        searchBoxChange()
-        bindEvent()
-        ListenScroll()
-        createCase()
+    function initPage(){
+        var submitData = {
+            
+        };
+        $.post('http://t6.miaoxing101.com//index.php?s=/home/index/anli', submitData,
+        function(data) {
+          createCase(data)
+          //添加搜索框变化动画
+          searchBoxChange()
+          //事件绑定
+          bindEvent()
+          //监听滚动条
+          ListenScroll()
+          $('.loading').css({display:'none',background:'rgba(0,0,0,0.7)'})
+          $('body').css('overflow','auto')
+        },"json")
     }
 
     //事件绑定函数
@@ -24,11 +35,13 @@ window.onload = (function(win) {
         $('.globalnav .search-box').on('click', function() {
                 $('.search-panel').animate({ right: '0' })
                 $('body').css('overflow', 'hidden')
+                $('.return').hide()
             })
         //取消按钮点击事件
         $('.search-box .cancel').on('click', function() {
             $('.search-panel').animate({ right: '-7.5rem' })
             $('body').css('overflow', 'auto')
+            $('.return').show()
         })
 
         //咨询按钮点击事件，弹出咨询框
@@ -46,7 +59,25 @@ window.onload = (function(win) {
             alert('aaa')
         })
 
-        bindCase()
+        //返回首页按钮点击事件
+        $('.return').on('click',function(){
+          $('.project-show').css('display','none')
+          $('.warpper').css('display','block')
+        })
+
+        //热词点击事件
+        $('#word-box ul li').on('click',function(){
+          var str = $(this).html()
+          search(str)
+          $('.return').show()
+        })
+
+        //搜索icon点击事件
+        $('#search-icon').on('click',function(){
+          var str = $(this).siblings().children().eq(0).val()
+          search(str)
+        })
+
     }
 
     //初始化swiper
@@ -64,6 +95,7 @@ window.onload = (function(win) {
                 slideShadows: true
             }
         })
+        isSwiper = false
     }
 
     //定时器监听页面滚动，搜索框变化
@@ -103,46 +135,85 @@ window.onload = (function(win) {
     }
 
     //生成案例
-    function createCase(){
-      var str = "<li><div class='outer'><img src='img/1.jpg' alt=''><div class='number'><div class='icon'><img src='img/yanjing.png' alt=''></div><span>186</span></div></div><p>瞄星案例</p></li>"
-      
+    function createCase(data){
+      console.log(data)
+      var title = ''
+      var view = ''
+      var src = ''
+      var str = ''
+      var strAll = ''
+      var id = 0
+      for(var i=0;i<data.list.length;i++){
+        title = data.list[i].title
+        view = data.list[i].view
+        id= data.list[i].id
+        src = "http://t6.miaoxing101.com/" + data.list[i].img
 
-      for(var i= 0;i<10;i++){
-        $('.plist').append(str)
-        bindCase()
+        str = "<li data-id="+id+"><div class='outer'><img src='"+src+"' alt=''><div class='number'><div class='icon'><img src='img/yanjing.png' alt=''></div><span>"+view+"</span></div></div><p>"+title+"</p></li>"
+
+        strAll = strAll + str
+
       }
+
+      $('.plist').append(strAll)
+      bindCase()
         
       
     }
 
     function bindCase(){
         //案例点击跳页事件
-        $('.plist li').one('click', function() {
-            $('.warpper').css('display', 'none')
-            $('.project-show').css('display', 'block')
-            window.scrollTo(0,0);
-            initSwiper()
+        $('.plist li').on('click', function() {
+            var id = $(this).data('id')
+            doProjectAjax(id)
         })
     }
 
+    function doProjectAjax(id){
+      var submitData = {
+            id:id
+        };
+        $.post('http://t6.miaoxing101.com//index.php?s=/home/index/detail', submitData,
+        function(data) {
+          Partculars(data)
+        },"json")
+    }
+
     //生成案例详情页
-    function Partculars(){
+    function Partculars(data){
 
+      console.log(data.detail)
+
+      var urlStr1 = 'url(http://t6.miaoxing101.com'+data.detail.imgpath[0]+')'
+      var urlStr2 = 'url(http://t6.miaoxing101.com'+data.detail.imgpath[1]+')'
+      var urlStr3 = 'url(http://t6.miaoxing101.com'+data.detail.imgpath[2]+')'
+
+      $('#project-title').html(data.detail.title)
+      $('#project-view').html(data.detail.view)
+      $('.portion1').css('background',urlStr1)
+      $('.portion2').css('background',urlStr2)
+      $('.portion3').css('background',urlStr3)
+
+
+
+      $('.warpper').css('display', 'none')
+      $('.project-show').css('display', 'block')
+      window.scrollTo(0,0);
+      if(isSwiper){
+        initSwiper()
+      }
     }
 
-
-
-    function doAjax(){
-      var url = 'http://t6.miaoxing101.com/index.php?s=/admin/database/index/index'
-      $.ajax({
-        url:url,
-        dataType:"json",
-        type:'get',
-        success:function(data){
-          cosole.log(data)
-        }
-      })
+    //搜索功能
+    function search(str){
+      // alert(str)
+      $('.search-panel').animate({ right: '-7.5rem' })
+      $('.project-show').hide()
+      $('.warpper').show()
+      $('.loading').show()
+      setTimeout(function(){
+        $('.loading').hide()
+      },1000)
     }
-
 
 })(window)
